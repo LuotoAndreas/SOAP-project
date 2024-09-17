4b474772f1be9738676c90d0d16db78ea4bfdd12
Andreas Luoto <luoto.andreas@gmail.com>
2024-09-17 22:11:50 +0300

    commit of 17.9

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,87 @@
-import React from "react";
-import "./App.css"; // Import the CSS file
+import React, {useEffect, useState} from "react"
+import CurrencyRow from './CurrencyRow'
+import "./App.css"
 
-const SearchBox = () => {
-  return (
-    <div className="search-box">
-      <div className="input-group">
-        <label>Destination</label>
-        <input type="text" placeholder="Enter destination" />
-        <button type="button">Search</button>
-      </div>
+const BASE_URL = 'https://open.er-api.com/v6/latest/USD'
 
-      <div className="input-group">
-        <label>Currency</label>
-        <input type="text" placeholder="Currency name" />
-        <select name="currency" id="currencies">
-          <option value="Euro">Euro</option>
-          <option value="Dollar">Dollar</option>
-          <option value="Pound">Pound</option>
-          <option value="Fran">Fran</option>
-          <option value="Yen">Yen</option>
-        </select>
-      </div>
-
-      <div className="input-group">
-        <label>To</label>
-        <select name="currency" id="currencies">
-          <option value="Euro">Euro</option>
-          <option value="Dollar">Dollar</option>
-          <option value="Pound">Pound</option>
-          <option value="Fran">Fran</option>
-          <option value="Yen">Yen</option>
-        </select>
-        <button type="button">Convert</button>
-      </div>
-
-      <div className="input-group">
-        <label>Temperature</label>
-        <input type="text" placeholder="Enter temperature" />
-        <div className="temp-buttons">
-          <button type="button">To Celsius</button>
-          <button type="button">To Fahrenheit</button>
-        </div>
-      </div>
-    </div>
-  );
-};
 
 const App = () => {
-  return (
-    <div className="app-container">
-      <h1>Travel Helper</h1>
-      <SearchBox />
+const[currencyOptions, setCurrencyOptions] = useState([])
+const [fromCurrency, setFromCurrency] = useState()
+const [toCurrency, setToCurrency] = useState()
+const [exchangeRate, setExchangeRate] = useState()
+const [amount, setAmount] = useState(1)
+const [amountInFromCurrency, setAmountInFromCurrency] = useState(true)
+
+let toAmount, fromAmount
+if (amountInFromCurrency) {
+  fromAmount = amount
+  toAmount = amount * exchangeRate
+} else {
+  toAmount = amount
+  fromAmount = amount / exchangeRate
+}
+
+useEffect(() => {
+  
+  fetch(BASE_URL)
+    .then(res => res.json())
+    .then(data => {
+      const firstCurrency = Object.keys(data.rates)[1]
+      setCurrencyOptions([data.base_code, ...Object.keys(data.rates)])
+      setFromCurrency(data.base_code)
+      setToCurrency(firstCurrency)
+      setExchangeRate(data.rates[firstCurrency])
+    })
+}, [])
+
+useEffect(() => {
+  if (fromCurrency != null && toCurrency != null){
+    fetch(`${BASE_URL}?base=${fromCurrency}&symbols=${toCurrency}`)
+    .then(res => res.json())
+    .then(data => setExchangeRate(data.rates[toCurrency]))
+  }  
+}, [fromCurrency, toCurrency])
+
+function handleFromAmountChange(e) {
+  setAmount(e.target.value)
+  setAmountInFromCurrency(true)
+}
+
+function handleToAmountChange(e) {
+  setAmount(e.target.value)
+  setAmountInFromCurrency(false)
+}
+
+  return (  
+    
+
+    <div className="Converter">
+      <div>
+        <h1>Destination</h1>   
+        <input type="letter" placeholder="The destination is..." />   
+      </div>
+      <h1>Currency exchanger</h1>
+      <CurrencyRow currencyOptions={currencyOptions} 
+      selectedCurrency={fromCurrency}
+      onChangeCurrency={e => setFromCurrency(e.target.value)}
+      onChangeAmount={handleFromAmountChange}
+      amount={fromAmount}
+      />
+      <div className="equals">=</div>
+      <CurrencyRow currencyOptions={currencyOptions} 
+      selectedCurrency={toCurrency}
+      onChangeCurrency={e => setToCurrency(e.target.value)}
+      onChangeAmount={handleToAmountChange}
+      amount={toAmount}
+      />
+      <div>
+        <h1>Weather</h1>   
+        <input type="letter" placeholder="The weather is..." />
+      </div>
     </div>
-  );
-};
+  )
+}
+
 
 export default App;
